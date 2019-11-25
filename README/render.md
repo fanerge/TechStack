46732fef6d236aa4f79a7abc24e73a10e2ecc003
zhenfan.yu <zhenfan.yu@mail.dealmoon.com>
2019-11-25 10:18:51 +0800

    feat: html js css

--- README/render.md
+++ README/render.md
@@ -13,6 +13,22 @@
 3.  async使JS下载变成异步，执行时机为下载后立即执行会阻塞HTML的解析
 4.  对WebKit 而言，仅当脚本尝试访问的样式属性可能受尚未加载的样式表影响时，它才会禁止该脚本。
 
+##  javascript对DOM树与CSSOM树创建的影响
+### 情形一，页面只有HTML 和 CSS
+CSS下载和解析都不阻塞DOM的解析<br>
+CSSOM和DOM形成RenderTree需要前面两个过程都完成
+### 情形二，页面只有HTML 和 JS（非外部）
+DOM解析过程遇到JS会被阻塞（因为JS可能会操作当前已经生成的DOM）<br>
+需要注意，如果JS操作还未生成的DOM节点（不生效）
+### 情形三，页面只有HTML 和 CSS 和 JS（均非外部）
+DOM解析时当遇到JS会被阻塞，JS执行之前需要等待CSSOM解析完成（因为JS可能会操作CSSOM）<br>
+所以说CSSOM在JS存在的情况下会间接阻塞DOM解析
+### 情形四，页面只有HTML 和 CSS 和 JS（均外部引入）
+Webkit渲染引擎有一个优化，在接收到HTML文件时会开启一个预解析线程，该线程会提前下载脚本、层叠样式表和图片<br>
+下载完后和情形三类似<br>
+
+[渲染树怎么形成的你真的很懂吗？](https://mp.weixin.qq.com/s?__biz=MzU3NjczNDk2MA==&mid=2247484783&idx=1&sn=75ded5947976a317ec07f6e15c1c02d9&chksm=fd0e16f0ca799fe616f9b6d4361548edd74b77be29b5dc594195f40791a08e1f8da98cb232ff&mpshare=1&scene=23&srcid&sharer_sharetime=1574643206426&sharer_shareid=3daf126008b83bc321fb3eeb3e318508%23rd)
+
 > 参考文档：
 [浏览器的工作原理](https://www.html5rocks.com/zh/tutorials/internals/howbrowserswork)
 [性能优化——CSS和JS的加载和执行](https://blog.csdn.net/qq_35534823/article/details/79356317)
--- src/utils/offer.js
+++ src/utils/offer.js
@@ -1,257 +1,29 @@
-// offer1
-const arr = [
-  [1, 2, 3],
-  [7, 8, 9],
-  [8, 9, 10]
-];
-function isInclude(arr, value) {
-  const min = arr[0][0];
-  const max = arr[arr.length -1].slice(-1);
-  if (value < min) {
-    return false;
-  }
-  if (value > max) {
-    return false;
-  }
-  // 找出每个数组最大值
-  const arrMax = arr.map((item) => item[item.length -1]); // 3, 4, 5
-  const arrMin = arr.map((item) => item[0]); // 1, 2, 7
-  const arrIndex = [];
-  for (let i = 0; i < arrMax.length; i++) {
-    if(arrMax[i] === value) {
-      return true;
-    }
-    if (arrMax[i] < value) {
-      arrIndex.push(i);
-    }
-  }
-  for (let i = 0; i < arrMin.length; i++) {
-    if(arrMin[i] === value) {
-      return true;
-    }
-    if (arrMin[i] > value) {
-      arrIndex.push(i);
-    }
-  }
-  // ...new Set(arrIndex)
-  const newArray = [];
-  [...new Set(arrIndex)].forEach(item => {
-    newArray.push(...arr[item]);
-  });
-
-  return newArray.includes(value);
-}
-// const test = isInclude(arr, 7);
-// console.log(test);
-
-// offer 2
-// We Are Happy => We%20Are%20Happy
-function spaceReplace (str) {
-  return str.replace(/\s/g, '%20');
-}
-// const str = 'We Are Happy';
-// const test = spaceReplace(str);
-// console.log(test);
-
-// offer（3）从尾到头打印链表
-// 栈的思想
-function tailPrint (head) {
-  let arr = [];
-  let node  = head;
-  while(node){
-    arr.unshift(node);
-    node = node.next;
-  }
-
-  return arr;
-}
-
-// offer（4）重建二叉树
-// 先序遍历（根左右）VLR
-// 中序遍历（左根右）
-// 后序遍历（左右根）
-// 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回
-function reConstructBinaryTree (pre, vin) {
-  if (pre.length === 0 || vin.length === 0) {
-    return null;
-  }
-  let index = vin.indexOf(pre[0]); // 根节点
-  let left = vin.slice(0, index); // 左子树
-  let right = vin.slice(index + 1); // 右子树
-
-  return {
-    val: pre[0],
-    left: reConstructBinaryTree(pre.slice(1, index + 1), left),
-    right: reConstructBinaryTree(pre.slice(index + 1), right)
-  };
-}
-// var pre = [1,2,4,7,3,5,6,8];
-// var vin = [4,7,2,1,5,3,8,6];
-// console.log(reConstructBinaryTree(pre, vin));
-
-// offer（5）用两个栈实现队列
-// 栈先进后出
-// 队列先进先出
-// 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
-// const outStack = [];
-// const inStack = [];
-// function push (node) {
-//   inStack.push(node);
-// }
-
-// function pop () {
-//   let len = inStack.length;
-//   if (len > 0) {
-//     for (let i = 0; i < len; i++) {
-//       outStack.push(inStack.pop());
-//     }
-//   }
-//   return outStack.pop();
-// }
-// const outStack = [],
-//   inStack = [];
-// function push(node) {
-//   // write code here
-//   inStack.push(node);
-// }
-// function pop() {
-//   // 每次都要outStack取完了再往里添加
-//   if (!outStack.length) {
-//     while (inStack.length) {
-//       outStack.push(inStack.pop());
-//     }
-//   }
-//   return outStack.pop();
-// }
-// push(1)
-// push(2)
-// push(4)
-// console.log(pop());
-// console.log(pop());
-// push(5);
-// push(6);
-// console.log(pop());
-// console.log(pop());
-// console.log(pop());
-// console.log(pop());
-
-
-// offer 6
-
-// offer 7 斐波那契数列 1 1 2 3 5 8 13 21
-// 递归方案
-// function offer7 (n) {
-//   if (n === 1 || n === 2) {
-//     return 1;
-//   }
-
-//   return offer7(n-1) + offer7(n-2);
-// }
-// console.log(offer7(6));
-// 动态规划
-// 动态规划的特点是：最优子结构、无后效性、子问题重叠。
-// 确定边界：F[1] = 1; F[2] = 1;
-// 状态转移：F[i] = F[i - 1] + F[i - 2]; 
-// function offer7 (n) {
-//   let n1 = 1;
-//   let n2 = 1;
-//   let n3 = 1;
-
-//   for (var i = 3; i <= n; i++) {
-//     n3 = n1 + n2;
-//     n1 = n2;
-//     n2 = n3;
-//   }
-
-//   return n3;
-// }
-// console.log(offer7(8))
-
-// 剑指offer（8）跳台阶
-// 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
-// a.假定第一次跳的是一阶，那么剩下的是n-1个台阶，跳法是f(n-1);
-// b.假定第一次跳的是2阶，那么剩下的是n-2个台阶，跳法是f(n-2)
-// c.由a\b假设可以得出总跳法为: f(n) = f(n-1) + f(n-2) 
-// d.然后通过实际的情况可以得出：只有一阶的时候 f(1) = 1 ,只有两阶的时候可以有 f(2) = 2
-function offer8(n){
-  let n1 = 1;
-  let n2 = 2;
-  let n3 = n === n1 ? n1 : n2;
-
-  for (let i = 3; i <= n; i++) {
-    n3 = n2 + n1;
-    n1 = n2;
-    n2 = n3;
-  }
-
-  return n3;
-}
-// console.log(offer8(5));
-
-// 剑指offer（9）变态跳台阶,一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
-// f(n) = 2f(n-1)
-// f(1) = 1;
-// f(2) = 2;
-// f(3) = 4;
-function offer9(n) {
-  if (n === 1) {
-    return 1;
-  }
-  let n1 = 1;
-  let temp;
-  for (let i = 2; i <= n; i++) {
-    temp = 2 * n1;
-    n1 = 2 * n1;
-  }
-
-  return temp;
-}
-// console.log(offer9(4));
-
-// 二分查找（非递归版本）
+/**
+ * 二分查找
+ * @param {*} ary 
+ * @param {*} target 
+ * @return {*} index || -1
+ */
 function binarySearch(ary, target) {
   let low = 0;
   let high = ary.length - 1;
+  // 必须要 low <= high ,因为存在 low = high = mid情况，这是需要 ary[mid] === targe 判断
   while(low <= high) {
-      let mid = parseInt((low + high) / 2, 10);
+      // let mid = parseInt((low + high) / 2, 10);
+      let mid = Math.floor((low + high)/2);
       if (target === ary[mid]) {
           return mid;
       }else if(target < ary[mid]) {
           high = mid - 1;
       }else if(target > ary[mid]) {
           low = mid + 1;
-      }else{
-          return -1;
       }
   }
+
+  return -1;
 }
 
-// 二分查找（递归版本）
-function binarySearch1(ary, low, high, target) {
-  if (low > high) {
-    return -1;
-  }
-  let mid = parseInt((low + high) / 2, 10);
-  if(ary[mid] === target){
-    return mid;
-  }else if(ary[mid] > target){
-    high = mid - 1;
-    return binarySearch1(ary, low, high, target);
-  }else if(ary[mid] < target){
-    low = mid + 1;
-    return binarySearch1(ary, low, high, target);
-  }else{
-    return -1;
-  }
-}
-
-var demo = binarySearch1([1, 2, 3, 5], 0, 4, 8);
-console.log(demo);
-
-
-
-
-
-
-
-
+// 原码、反码、补码
+// 正数的原码反码补码相同
+// 负数的反码：除符号位外,原码各位取反
+// 负数的补码：除符号位外,原码各位取反,再加1（即反码+1）
