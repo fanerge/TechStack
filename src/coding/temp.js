11509a563b36760f0c4940f76ae3b2aecea81cf6
fanerge@qq.com <fanerge@qq.com>
2021-02-02 17:01:58 +0800

    fix: deepclone

--- src/coding/temp.js
+++ src/coding/temp.js
@@ -118,34 +118,70 @@ function find(list, target) {
 // var c = find([7, 7, 8, 9, 1, 2, 5, 6, 7, 7], 8);
 // console.log(a, b, c);
 
+// 1. 针对能够遍历对象的不可枚举属性以及 Symbol 类型，我们可以使用 Reflect.ownKeys 方法；
+// 2. 当参数为 Date、RegExp 类型，则直接生成一个新的实例返回；
+// 3. 利用 Object 的 getOwnPropertyDescriptors 方法可以获得对象的所有属性，以及对应的特性，顺便结合 Object 的 create 方法创建一个新对象，并继承传入原对象的原型链；
+// 4. 利用 WeakMap 类型作为 Hash 表，因为 WeakMap 是弱引用类型，可以有效防止内存泄漏（你可以关注一下 Map 和 weakMap 的关键区别，这里要用 weakMap），作为检测循环引用很有帮助，如果存在循环，则引用直接返回 WeakMap 存储的值。
+
+// 下面是验证代码
+let obj = {
+  num: 0,
+  str: '',
+  boolean: true,
+  unf: undefined,
+  nul: null,
+  obj: { name: '我是一个对象', id: 1 },
+  arr: [0, 1, 2],
+  func: function () { console.log('我是一个函数') },
+  date: new Date(0),
+  reg: new RegExp('/我是一个正则/ig'),
+  [Symbol('1')]: 1,
+};
+Object.defineProperty(obj, 'innumerable', {
+  enumerable: false, value: '不可枚举属性' }
+);
+obj = Object.create(obj, Object.getOwnPropertyDescriptors(obj))
+obj.loop = obj    // 设置loop成循环引用的属性
+
+
 // deepClone
-function isBasicType(val) {
-  let list = ["undefined", "number", "string", "boolean", "symbol", "bigint"];
-  return val === null || list.includes(typeof val);
+function isComplexType(val) {
+  return (typeof val === 'object' && val !== null) || (typeof val === 'function')
 }
-function isObject(val) {
-  return Object.prototype.toString.call(val).slice(8, -1) === "Object";
-}
-function isArray(val) {
-  return Array.isArray(val);
-}
-function deepClone(old, map = new WeakMap()) {
-  // 基本类型，null、undefined、number、string、boolean、symbol、bigint
-  if (isBasicType(old)) {
-    return old;
-  }
-  // object\array
-  if (map.has(old)) {
-    return map.get(old);
+function deepClone(oldVal, hash = new WeakMap()) {
+  // 基本类型
+  if(!isComplexType(oldVal)) {
+    return oldVal;
   }
 
-  let newVal = new old.constructor();
-  map.set(newVal, newVal);
-  Reflect.ownKeys(old).forEach((key) => {
-    if (isObject(old[key]) || isArray(old[key])) {
-      newVal[key] = deepClone(old[key], map);
-    } else {
-      newVal[key] = old[key];
+  // 引用类型
+  let constructor = oldVal.constructor
+  // Date
+  if(constructor === Date) {
+    return new Date(oldVal)
+  }
+  // RegExp
+  if(constructor === RegExp) {
+    return new RegExp(oldVal)
+  }
+  // Function 
+  if(constructor === Function) {
+    // 闭包原理
+    return new Function(`return ${oldVal.toString()}`)()
+  }
+  if(hash.has(oldVal)) {
+    return hash.get(oldVal)
+  }
+  let newVal = new constructor()
+  Object.setPrototypeOf(newVal, Object.getPrototypeOf(oldVal))
+  hash.set(oldVal, newVal);
+  
+  // Object.getOwnPropertyNames\Object.getOwnPropertySymbols
+  Reflect.ownKeys(oldVal).forEach(item => {
+    if(isComplexType(oldVal[item])) {
+      newVal[item] = deepClone(oldVal[item], hash)
+    }else{
+      Object.defineProperty(newVal, item, Object.getOwnPropertyDescriptor(oldVal, item))
     }
   });
 
